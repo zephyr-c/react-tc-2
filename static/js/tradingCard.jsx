82db10b248c46a92228a2a97ac1b9d5a03330045
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-31 18:08:08 -0700

    Did the things!

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,34 +1,92 @@
-var tradingCardData = [
-  {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg'
-  },
+// var tradingCardData = [
+//   {
+//     name: 'Balloonicorn',
+//     skill: 'video games',
+//     imgUrl: '/static/img/balloonicorn.jpg'
+//   },
 
-  {
-    name: 'Float',
-    skill: 'baking pretzels',
-    imgUrl: '/static/img/float.jpg'
-  },
+//   {
+//     name: 'Float',
+//     skill: 'baking pretzels',
+//     imgUrl: '/static/img/float.jpg'
+//   },
 
-  {
-    name: 'Llambda',
-    skill: 'knitting scarves',
-    imgUrl: '/static/img/llambda.jpg'
-  }
-];
+//   {
+//     name: 'Llambda',
+//     skill: 'knitting scarves',
+//     imgUrl: '/static/img/llambda.jpg'
+//   }
+// ];
 
 class TradingCard extends React.Component {
   render() {
     return (
       <div className="card">
         <p>Name: {this.props.name}</p>
-        <img src={this.props.imgUrl} />
-        <p>Skill: {this.props.skill} </p>
+        <img src={this.props.imgUrl} id="cardimg" />
+        <div id="skill"><p>Skill: {this.props.skill} </p></div>
       </div>
     );
   }
 }
+
+class TradingCardForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      skill: '',
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSkillChange = this.handleSkillChange.bind(this);
+    this.addNewCard = this.addNewCard.bind(this);
+  }
+
+  addNewCard() {
+    const data = {
+      name: this.state.name,
+      skill: this.state.skill
+    };
+    $.post('/add-card', data, this.updateCards);
+  }
+
+  updateCards() {
+    // $.get('/cards.json', this.props.handleNewCardAdded);
+    alert('Add a card?!?')
+  }
+
+  handleNameChange(evt) {
+    this.setState({ name: evt.target.value });
+  }
+
+  handleSkillChange(evt) {
+    this.setState({ skill: evt.target.value });
+  }
+
+  render() {
+    return (
+      <form>
+        <label for="name">Name:</label>
+        <input 
+          id="name"
+          type="text"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
+        <label for="skill">Skill:</label>
+        <input
+          id="skill"
+          type="text"
+          value={this.state.skill}
+          onChange={this.handleSkillChange}
+        />
+        <button onClick={this.addNewCard}>Add</button>
+      </form>
+    );
+  }
+}
+
 
 class TradingCardContainer extends React.Component {
   constructor() {
@@ -44,6 +102,7 @@ class TradingCardContainer extends React.Component {
 
   updateCards(response) {
     const cards = response.cards;
+    console.log(cards)
     this.setState({ cards: cards });
   }
 
@@ -66,7 +125,10 @@ class TradingCardContainer extends React.Component {
     }
 
     return (
-      <div>{tradingCards}</div>
+      <div>
+        <TradingCardForm handleNewCardAdded={this.updateCards} />
+        <div>{tradingCards}</div>
+      </div>
     );
   }
 }
